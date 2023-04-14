58c7116f1a41f0b8da103e1be3658968dd4d2535
etr1x <etr1x@gmail.com>
2023-04-14 23:27:29 +1000

    Added MakeAppointment frame, edited UserMenu

--- Client/screen.py
+++ Client/screen.py
@@ -6,6 +6,7 @@ from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
 from asciimatics.effects import Cycle, Stars
 from asciimatics.renderers import FigletText
 
+from datetime import datetime
 import sys
 import re
 import sqlite3
@@ -36,7 +37,36 @@ class LoginModel(object):
         #     )      
         # ''')
         # self._db.commit()
+        
+        # self._db.cursor().execute('''
+        #     CREATE TABLE medics(
+        #     id INTEGER PRIMARY KEY,
+        #     name TEXT,
+        #     surname TEXT,
+        #     specialty TEXT,
+        #     cabinet INTEGER
+        #     )
+        # ''')
+        # self._db.commit()
 
+        # self._db.cursor().execute('''
+        #     CREATE TABLE priemtimes(
+        #     id INTEGER PRIMARY KEY,
+        #     medic_id INTEGER,
+        #     time TEXT
+        #     )        
+        # ''')
+        # self._db.commit()
+        
+        # self._db.cursor().execute('''
+            # CREATE TABLE user_priems(
+            # id INTEGER PRIMARY KEY,
+            # user_id INTEGER,
+            # priem TEXT
+            # )                      
+        # ''')
+        # self._db.commit()
+        
         # Current contact when editing.
         self.current_id = None
 
@@ -80,22 +110,53 @@ class LoginModel(object):
         else:
             return self.get_user(self.current_id)
 
+    def get_medics_specialty(self):
+        return self._db.cursor().execute(
+            "SELECT specialty, id from medics").fetchall()
+        
+    def get_medic_name(self, medic_id):
+        return self._db.cursor().execute(
+            "SELECT name, surname, cabinet from medics WHERE id=:id", {"id": medic_id}).fetchall()
+        
+    def get_medic_time(self, medic_id):
+        return self._db.cursor().execute(
+            "SELECT time, id from priemtimes WHERE medic_id=:id", {"id": medic_id}).fetchall()
+        
+    def get_medic_specialty(self, medic_id):
+        return self._db.cursor().execute(
+            "SELECT specialty from medics WHERE id=:id", {"id": medic_id}).fetchall()
+        
+    def get_medic_time_by_id(self, medic_id):
+        return self._db.cursor().execute(
+            "SELECT time from priemtimes WHERE medic_id=:id", {"id": medic_id}).fetchall()
+    
+    def make_appointment(self, priem):
+        self._db.cursor().execute('''
+            INSERT into user_priems(user_id, priem)
+            VALUES(:user_id, :priem)''',
+                                    priem)
+        self._db.commit()
+    
+    def get_appointments(self, user_id):
+        return self._db.cursor().execute(
+            "SELECT priem, id from user_priems WHERE user_id=:id", {"id": user_id}).fetchall()
+    
     # def delete_contact(self, contact_id):
     # self._db.cursor().execute('''
     #     DELETE FROM contacts WHERE id=:id''', {"id": contact_id})
     # self._db.commit()
 
-class Greetings(Frame):
-    def __init__(self, screen, model):
-        super(LoginMenu, self).__init__(screen,
-                                        screen.height * 2 // 3,
-                                        screen.width * 2 // 3,
-                                        can_scroll=False,
-                                        )
-        self._model = model
+# class Greetings(Frame):
+#     def __init__(self, screen, model):
+#         super(LoginMenu, self).__init__(screen,
+#                                         screen.height * 2 // 3,
+#                                         screen.width * 2 // 3,
+#                                         can_scroll=False,
+#                                         )
+#         self._model = model
 
-    def _next(self):
-        raise NextScene("LoginMenu")
+#     def _next(self):
+#         raise NextScene("LoginMenu")
 
 
 class LoginMenu(Frame):
@@ -143,15 +204,13 @@ class LoginMenu(Frame):
 
     def _login(self):
         self.save()
-        self.writen_email = self.data["email"]
-        self.written_pass = self.data["pass"]
         self.itr = 0
         self.usrs = self._model.get_auth_info()
         for row in self.usrs:
             self.id = row[2]
             self.email = row[0]
             self.passw = row[1]
-            if (self.writen_email == self.email) and (self.written_pass == self.passw):
+            if (self.data["email"] == self.email) and (self.data["pass"] == self.passw):
                 self.itr += 1
                 self._model.current_id = self.id
                 raise NextScene("User Menu")
@@ -242,6 +301,7 @@ class RegisterMenu(Frame):
                      value)
         return len(value) == 0 or m is not None
 
+
 class UserMenu(Frame):
     def __init__(self, screen, model):
         super(UserMenu, self).__init__(screen,
@@ -256,10 +316,10 @@ class UserMenu(Frame):
 
         layout = Layout([100], fill_frame=False)
         self.add_layout(layout)
-        layout.add_widget(Divider(draw_line=False, height=2))
-        layout.add_widget(Label(label="ГОСУСЛУГИ", align="^", height=3))
+        layout.add_widget(Divider(draw_line=False, height=1))
+        layout.add_widget(Label(label="ГОСУСЛУГИ", align="^", height=1))
 
-        layoutmain = Layout([40,10,40], fill_frame=True)
+        layoutmain = Layout([1], fill_frame=True)
         self.add_layout(layoutmain)
 
         self._email = Text("E-mail:", "email", readonly=True)
@@ -269,22 +329,28 @@ class UserMenu(Frame):
         self._polis = Text("Полис:", "polis", readonly=True)
         self._age = Text("Возраст:", "age", readonly=True)
 
-        layoutmain.add_widget(Label(label="Ваши данные:", align="<"),0)
-        layoutmain.add_widget(self._email,0)
-        layoutmain.add_widget(self._name,0)
-        layoutmain.add_widget(self._surname,0)
-        layoutmain.add_widget(self._number, 0)
-        layoutmain.add_widget(self._polis, 0)
-        layoutmain.add_widget(self._age, 0)
-        layoutmain.add_widget(Divider(draw_line=False))
-        layoutmain.add_widget(Label("Ваши приемы:", align="<"), 2)
-        layoutbutton = Layout([1,1])
+        layoutmain.add_widget(Label(label="Ваши данные:", align="<"))
+        layoutmain.add_widget(self._email)
+        layoutmain.add_widget(self._name)
+        layoutmain.add_widget(self._surname)
+        layoutmain.add_widget(self._number)
+        layoutmain.add_widget(self._polis)
+        layoutmain.add_widget(self._age)
+        layoutmain.add_widget(Divider())
+        layoutmain.add_widget(Label("Ваши приемы:", align="<"), 0)
+        self._appointments = ListBox(Widget.FILL_FRAME, options=self._model.get_appointments(self._model.current_id), add_scroll_bar=True)
+        layoutmain.add_widget(self._appointments)
+        layoutmain.add_widget(Divider())
+        layoutbutton = Layout([1,1,1])
         self.add_layout(layoutbutton)
-        layoutbutton.add_widget(Button("Выход", self._exit), 1)
+        layoutbutton.add_widget(Button("Выход", self._exit), 2)
+        layoutbutton.add_widget(Button("Записаться к врачу", self._appointment), 1)
         layoutbutton.add_widget(Button("Изменение данных", self._change_data), 0)
-        self.fix()
+        self.fix()      
 
-    def _reload(self):
+    def _reload(self, new_value = None):
+        self._appointments.options = self._model.get_appointments(self._model.current_id)
+        self._appointments.value = new_value
         self._params = self._model.get_current_user()
         for row in self._params:
             self._email.value = row[0]
@@ -293,6 +359,9 @@ class UserMenu(Frame):
             self._number.value = str(row[3])
             self._polis.value = str(row[4])
             self._age.value = str(row[5])
+    
+    def _appointment(self):
+        raise NextScene("Make appointment")
 
     def _change_data(self):
         raise NextScene("User change data")
@@ -356,12 +425,94 @@ class UserChangeMenu(Frame):
     def _dont_save_return(self):
         raise NextScene("User Menu")
 
+class MakeAppointment(Frame):
+    def __init__(self, screen, model):
+        super(MakeAppointment, self).__init__(screen,
+                                        screen.height * 2 // 2,
+                                        screen.width * 2 // 3,
+                                        hover_focus=True,
+                                        can_scroll=False,
+                                        title="Запись к врачу",
+                                        reduce_cpu=True)
+        self._model = model
+
+        layout = Layout([100], fill_frame=False)
+        self.add_layout(layout)
+        layout.add_widget(Divider(draw_line=False, height=2))
+        layout.add_widget(Label(label="Запись", align="^", height=3))
+
+        layoutmain = Layout([20,60,20],fill_frame=True)
+        self.add_layout(layoutmain)
+        layoutmain.add_widget(Divider(False, 1), 0)
+        self._specialty_choose = DropdownList(label="Специализация", options=self._model.get_medics_specialty(), on_change=self._show_medic_name_and_cab, fit=True)
+        layoutmain.add_widget(self._specialty_choose, 1)
+        self._medic_name = Text(label="Ваш врач:", name="medic_name", readonly=True, max_length=30)
+        layoutmain.add_widget(self._medic_name, 1)
+        self._medic_cab = Text(label="Кабинет:", name="cab", readonly=True, max_length=3)
+        layoutmain.add_widget(self._medic_cab, 1)
+        layoutmain.add_widget(Divider(False, 1), 2)
+        self._time_choose = DropdownList(label="Время", options=[("/Выберите врача/", 1)], fit=True)
+        layoutmain.add_widget(self._time_choose, 1)
+        #layoutmain.add_widget(self._medic_choose, 1)
+        #layoutmain.add_widget(self._time_choose, 2)
+
+        layoutbuttons = Layout([1,2])
+        self.add_layout(layoutbuttons)
+        self._make_appointment_button = Button("Записаться", self._make_appointment)
+        self._return_button = Button("Выйти", self._return)
+        layoutbuttons.add_widget(self._make_appointment_button, 0)
+        layoutbuttons.add_widget(self._return_button, 1)
+        self.fix()
+        
+
+    def _show_medic_name_and_cab(self):
+        self.id = self._specialty_choose.value
+        self.medic = self._model.get_medic_name(self.id)
+        for row in self.medic:
+            self.medic_name = row[0]
+            self.medic_surname = row[1]
+            self.medic_cab = row[2]
+        self.medic_fullname = self.medic_name + ' ' + self.medic_surname
+        self._medic_name.value = self.medic_fullname
+        self._medic_cab.value = str(self.medic_cab)
+        self._time_choose.options = self._model.get_medic_time(self.id)
+        
+            
+    
+    def _make_appointment(self):
+        self.save()
+        self.id = self._specialty_choose.value
+        self.specialty_temp = self._model.get_medic_specialty(self.id)
+        for row in self.specialty_temp:
+            self.specialty = row[0]
+        self.time_temp = self._model.get_medic_time_by_id(self.id)
+        for row in self.time_temp:
+            self.time = row[0]
+        self._appointment_text = self.specialty + ' ' + self.data["medic_name"] + ' в кабинете ' + self.data["cab"] + ' в ' + self.time
+        self._appointment = {"user_id": self._model.current_id, "priem": self._appointment_text}
+        self._model.make_appointment(self._appointment)
+        raise NextScene("User Menu")
+        
+
+    def _return(self):
+        raise NextScene("User Menu")
+    
+
+
+
+
+
+
+
+
+
 def demo(screen, scene):
     scenes = [
         Scene([LoginMenu(screen, users)], -1, name="Login"),
         Scene([RegisterMenu(screen, users)], -1, name="Register"),
         Scene([UserMenu(screen, users)], -1, name="User Menu"),
-        Scene([UserChangeMenu(screen, users)], -1, name="User change data")
+        Scene([UserChangeMenu(screen, users)], -1, name="User change data"),
+        Scene([MakeAppointment(screen, users)], -1, name="Make appointment")
     ]
     screen.play(scenes, stop_on_resize=True, start_scene=scene, allow_int=True)
 
