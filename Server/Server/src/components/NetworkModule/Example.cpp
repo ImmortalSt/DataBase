26d67bec07869a851f6906566b2563973af39077
N1K0L41M4 <120606632+MasalovNIkolai@users.noreply.github.com>
2023-04-22 23:40:46 +1000

    Create NetworkModule.h, Example.cpp, example.py
    В NetworkModule.h:
     - Создал класс Network, задал ему параметры: socket server, listen_sock, addr, msg. Добавил методы:
    setMsg - создание сообщения на отправку клиента,
    receiveRequest - получение строки, отправленной клиентом,
     sendRequest - отправить ответ (msg) клиенту,
    bindSock - установить сокету адрес и порт,
    listenSock - установить режим прослушивания сокета,
    acceptSock  - возвращает клиентский сокет.
    Example.cpp - пример использования сервера
    example.py - пример использования клиента

---
+++ Server/Server/src/components/NetworkModule/Example.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#pragma warning(disable:4996)
+#include <stdlib.h>
+#include <time.h>
+#include "src/components/NetworkModule/NetworkModule.h"
+using namespace std;
+
+int main() {
+		WSAData wsaData;
+		WORD DLLVersion = MAKEWORD(2, 1);
+		if (WSAStartup(DLLVersion, &wsaData) != 0) {
+			cout << "Error" << endl;
+		}
+		string alphabet = "AaBbCcDdEeFfGgJjKkLlMmNnOoPpQqRrSsTZ0123456789";
+		srand(time(NULL));
+		Network serv = Network();
+		serv.bindSock();
+		serv.listenSock();
+		cout << "<<SERVER FIELD>>" << endl<< endl;
+		while (TRUE) {
+			
+			serv.acceptSock();
+			string rec = serv.receiveRequest();
+			
+			for (int i = 0; i < rec.size(); i++) {
+				int r = rand() % (alphabet.size());
+				rec[i] = alphabet[r];
+			}
+			char const* msg = rec.c_str();
+			cout << "Server convert recieved msg to: " << msg << endl;
+			serv.setMsg(msg);
+			serv.sendRequest();
+			cout << endl;
+		}
+}
+
---
+++ Server/Server/src/components/NetworkModule/NetworkModule.h
@@ -0,0 +1,113 @@
+#ifndef NETWORKMODULE_H;
+//#define NETWORKMODULE_H;
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#pragma warning(disable:4996)
+#include <stdlib.h>
+using namespace std;
+
+class INetwork {
+public:
+	virtual int bindSock() = 0;
+	virtual int sendRequest() = 0;
+	virtual string receiveRequest() = 0;
+};
+
+class PlugNetwork : INetwork {
+	int bindSock() override {
+		return 1;
+	}
+	int sendRequest() override {
+		return 0;
+	}
+	string receiveRequest() override {
+		return "Ivan Ivanov";
+	}
+
+};
+
+class Network : INetwork {
+public:
+
+	Network() {
+		addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+		addr.sin_port = htons(1111);
+		addr.sin_family = AF_INET;
+		server = socket(AF_INET, SOCK_STREAM, NULL);
+	}
+
+	Network(const char* address, int port) {
+		addr.sin_addr.s_addr = inet_addr(address);
+		addr.sin_port = htons(port);
+		addr.sin_family = AF_INET;
+		server = socket(AF_INET, SOCK_STREAM, NULL);
+	}
+
+	void acceptSock() {
+		int sizeofaddr = sizeof(addr);
+		listen_Sock = accept(server, (SOCKADDR*)&addr, &sizeofaddr);
+	}
+
+	void listenSock() {
+		listen(server, SOMAXCONN);
+	}
+
+	int bindSock() override {
+		int result = bind(server, (SOCKADDR*)&addr, sizeof(addr));
+		if (result == SOCKET_ERROR) {
+			cout << "building socket failed, error: " << result << endl;
+			WSACleanup();
+		}
+		else
+			return 0;
+	}
+
+	int sendRequest() override {
+		int result = send(listen_Sock, msg, strlen(msg), NULL);
+		if (result == SOCKET_ERROR) {
+			cout << "send failed, error: " << result << endl;
+			closesocket(listen_Sock);
+			WSACleanup();
+			return result;
+		}
+		else
+			return 0;
+	}
+
+	string receiveRequest() override {
+		char recvBuffer[512];
+		ZeroMemory(recvBuffer, 512);
+
+		int result = recv(listen_Sock, recvBuffer, size(recvBuffer), 0);
+		string recvBuff(recvBuffer);
+
+		if (result > 0) {
+			cout << "Recieved bytes: " << result << endl;
+			cout << "Recieved data: " << recvBuff << endl;
+			return recvBuff;
+		}
+		else if (result == 0)
+			cout << "Connection closing" << endl;
+		else {
+			cout << "Recieved failed" << endl;
+			closesocket(listen_Sock);
+			WSACleanup();
+		}		
+	}
+
+	void setMsg(char const* _msg) {
+		msg = _msg;
+	}
+
+private:
+	char const* msg;
+	SOCKET server;
+	SOCKET listen_Sock;
+	SOCKADDR_IN addr;
+};
+
+#endif
+
+
+
