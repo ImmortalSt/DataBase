8686a119d0b467eb5069117ff9327d42d51192e7
aerzex <113291689+aerzex@users.noreply.github.com>
2023-05-03 19:30:34 +1000

    Made raw function StarLoop

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -1,38 +1,54 @@
 ﻿#include "../Server/src/components/container/usersContainer.h"
 #include "../Server/src/components/container/medicsContainer.h"
 #include "../Server/src/components/container/priem_timesContainer.h"
-#include "../Server/src/components/container/users_priems.h"
+#include "../Server/src/components/container/users_priemsContainer.h"
 #include <iostream>
 #include "src/components/NetworkModule/NetworkModule.h"
 #include <nlohmann/json.hpp>
 
 
+void StartLoop(INetwork* network);
 
 void main(string from_client) {
 	INetwork* network = (INetwork*)new Network();
-	MedicsContainer* medics = new MedicsContainer;
-	UsersContainer* users = new UsersContainer;
-	Priem_timesContainer* tm_priems = new Priem_timesContainer;
-	User_priemsContainer* us_priems = new User_priemsContainer;
+	
+	
+	int a = 0;
+	StartLoop(network);
+}
+
+void StartLoop(INetwork* network) {
 	while (true) {
+		UsersContainer* users{};
+		MedicsContainer* medics{};
+		PriemTimesContainer* tmpriems{};
+		User_PriemsContainer* uspriems{};
+		
 		std::string buffer = network->receiveRequest();
 		std::cout << buffer;
-		nlohmann::json recv =  nlohmann::json::parse(buffer);
-
-		if (recv["Method"] == "INSERT") {
-			std::cout << "\n ddd:" << recv["DBname"];
+		nlohmann::json recv = nlohmann::json::parse(buffer);
+		if (recv["DBName"] == "users") {
+			if (recv["Method"] == "INSERT") {
+				users->addElement(recv["param"]);
+			}
 		}
-		
-		
-		
-
-		
-
-		
-		
-		
-		
+		else if (recv["DBName"] == "medics") {
+			if (recv["method"] == "INSERT") {
+				medics->addElement(recv["param"]);
+			}
+		}
+		else if (recv["DBName"] == "priemtimes") {
+			if (recv["method"] == "INSERT") {
+				tmpriems->addElement(recv["param"]);
+			}
+		}
+		else if (recv["DBName"] == "user_priems") {
+			if (recv["method"] == "INSERT") {
+				uspriems->addElement(recv["param"]);
+			}
+		}
+		else {
+			cout << "\n Incorrect  DBNAME\n";
+		}
 	}
-	int a = 0;
-	
 }
